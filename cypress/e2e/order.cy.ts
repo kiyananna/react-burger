6fb81a03e7e164e6ac6033bf9122651b11377a63
kiyananna <53748440+kiyananna@users.noreply.github.com>
2024-01-21 20:07:01 +0300

    sprint-6/step-1

--- cypress/e2e/order.cy.ts
+++ cypress/e2e/order.cy.ts
@@ -3,15 +3,22 @@ describe('Проверка добавления и отправки', () => {
     cy.intercept("GET", "api/auth/user", { fixture: "user.json" });
     cy.intercept("POST", "api/orders", { fixture: "order.json" }).as("postOrder");
 
+    window.localStorage.setItem("accessToken", "test-accessToken");
     window.localStorage.setItem(
       "refreshToken",
       JSON.stringify("test-refreshToken")
     );
+    window.localStorage.setItem("accessToken", "test-accessToken");
 
     cy.visit("http://localhost:3000");
   });
   
   it('Добавление и отправка', () => {
+    cy.visit('http://localhost:3000/login');
+    cy.get('input').first().should('exist').type('anka-kiynka1995@mail.ru')
+    cy.get('input').last().should('exist').type('mamalove07')
+    cy.get('button').contains('Войти').click();
+
     cy.get('[data-test="Краторная булка N-200i"]').trigger('dragstart');
     cy.get('[data-test="BurgerConstructor"]').trigger('drop');
     cy.get('[data-test="Краторная булка N-200i"]').trigger('dragover');
